5459b2e46a53da0570987986ab2b949d75af65ca
sfaugere <satnislasf@orange.fr>
2022-04-13 00:27:03 +0200

    re

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
-/*
+
 char *ft_strncpy(char *dest, char *src, unsigned int n)
 {
     unsigned int i = 0;
 
-    while(src[i] != '\0')
+    while(src[i] != '\0' && i < n)
     {
         dest[i] = src[i];
 
@@ -18,19 +18,9 @@ char *ft_strncpy(char *dest, char *src, unsigned int n)
 
     return dest;
 }
-*/
 
-char *ft_strncpy(char *dest, char *src, unsigned int n)
-{
-    unsigned int i = 0 ;
 
-    while(src[i] != '\0' && i < n)
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-}
+
 
 
 /*/
