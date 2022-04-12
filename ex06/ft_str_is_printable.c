d3b54031547b9311fadc228a867f94c1ca10c1ea
sfaugere <satnislasf@orange.fr>
2022-04-13 00:09:23 +0200

    re

--- ex06/ft_str_is_printable.c
+++ ex06/ft_str_is_printable.c
@@ -12,7 +12,7 @@ int ft_str_is_printable(char *str)
     
     while(str[i] != '\0')
     {
-        if(str[i] >= 33 && str[i] <= 126)
+        if(str[i] >= 32 && str[i] <= 126)
         {
             i++;
         }
